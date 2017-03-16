b49c963cce5f162d6d0cca06e88a69c59e428e22
vcabales <vcabales96@gmail.com>
2017-03-16 16:10:33 -0400

    Tabs functioning now

--- animations.js
+++ animations.js
@@ -1,56 +1,10 @@
-//get the modals
-var resumeModal = document.getElementById("resume");
-var codeModal = document.getElementById("code");
-var writingModal = document.getElementById("writing");
-var photoModal = document.getElementById("photo");
-var modal = document.getElementsByClassName("modal");
-
-// Get the button that opens the modal
-var resumeBtn = document.getElementById("resumeButton");
-var codeBtn = document.getElementById("codeButton");
-var wrtngBtn = document.getElementById("writingButton");
-var photoBtn = document.getElementById("photoButton");
-
-resumeBtn.onclick = function() {
-  resumeModal.style.display = "block";
-}
-
-codeBtn.onclick = function() {
-  codeModal.style.display = "block";
-}
-
-wrtngBtn.onclick = function() {
-  writingModal.style.display = "block";
-}
-
-photoBtn.onclick = function() {
-  photoModal.style.display = "block";
-}
-          
-// Get the <span> element that closes the modal
-var closeResume = document.getElementById("closeResume");
-var closeCode = document.getElementById("closeCode");
-var closeWriting = document.getElementById("closeWriting");
-var closePhoto = document.getElementById("closePhoto");
-
-// When the user clicks on <span> (x), close the modal
-
-closeResume.onclick = function() {
-  resumeModal.style.display = "none";
-}
-
-closeCode.onclick = function() {
-  codeModal.style.display = "none";
-}
-
-closeWriting.onclick = function() {
-  writingModal.style.display = "none";
-}
-
-closePhoto.onclick = function() {
-  photoModal.style.display = "none";
-}
-
-//Add (esc button)
-
+$(document).ready(function() {
+$('#menu a[href="#home"]').tab('show');
+$('#menu a').click(function (e) {
+  $(this).tab('show')
+}); 
+$('#menu a').on('hidden.bs.tab', function(event){
+	$(event.target).removeClass('active')
+});
+});
 
